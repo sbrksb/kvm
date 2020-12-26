2503b0789b25b4b9713256696678479b402811c7
Elena Afanasova <eafanasova@gmail.com>
2020-12-26 21:13:49 +0300

    virt/kvm: fix headers
    Signed-off-by: Elena Afanasova <eafanasova@gmail.com>

--- virt/kvm/eventfd.h
+++ virt/kvm/eventfd.h
@@ -4,6 +4,11 @@
 
 #ifdef CONFIG_KVM_IOREGION
 bool kvm_eventfd_collides(struct kvm *kvm, int bus_idx, u64 start, u64 size);
+#else
+static inline bool
+kvm_eventfd_collides(struct kvm *kvm, int bus_idx, u64 start, u64 size)
+{
+	return false;
+}
 #endif
-
 #endif
--- virt/kvm/ioregion.h
+++ virt/kvm/ioregion.h
@@ -5,6 +5,11 @@
 #ifdef CONFIG_KVM_IOREGION
 inline bool overlap(u64 start1, u64 size1, u64 start2, u64 size2);
 bool kvm_ioregion_collides(struct kvm *kvm, int bus_idx, u64 start, u64 size);
+#else
+static inline bool
+kvm_ioregion_collides(struct kvm *kvm, int bus_idx, u64 start, u64 size)
+{
+	return false;
+}
 #endif
-
 #endif
